0a940082000f97f02b8e1227bae78821b35bbc3b
ualjgm847 <jorgegmmgg@gmail.com>
2024-12-11 21:34:06 +0100

    banner cambiado

--- view/index.php
+++ view/index.php
@@ -5,10 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clínica Rayo</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
-        
+        <img class="logo" src="img/LogoBlanco.png" alt="Logo">
     </div>
 </body>
 </html>
