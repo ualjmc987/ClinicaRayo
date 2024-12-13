4a03cb09e7dded9c411458cba4da93f03ac834be
javisan2711 <javisan2711@gmail.com>
2024-12-13 11:16:43 +0100

    panelAdmin

--- vista/panelAdmin.php
+++ vista/panelAdmin.php
@@ -27,7 +27,142 @@
             border-radius: 0px;
         }
 
-        .contenidoCuadro2 {
+        .contenidoCuadro1 .titulo1{
+
+            margin: 2;
+            font-size: 48px;
+            font-weight: 700;
+            margin-bottom: 10px;
+            position: relative;
+            left: 0px;
+            top: 70px;
+            color: white;
+        }
+
+        .contenidoCuadro1 .botones1{
+            width: 267px;  
+            height: 100px;  
+            border: none;  
+            font-size: 32px;  
+            font-weight: bold;
+            cursor: pointer;
+            text-transform: none; 
+            color: black; 
+            background-color:white;
+            border-radius: 5px; 
+            transition: background-color 0.3s, transform 0.2s;
+            position:relative;
+            top: 100px;
+            left: -150px;
+        }
+
+        .contenidoCuadro1 .botones2{
+            width: 267px;  
+            height: 100px;  
+            border: none;  
+            font-size: 32px;  
+            font-weight: bold;
+            cursor: pointer;
+            text-transform: none; 
+            color: black; 
+            background-color:white;
+            border-radius: 5px; 
+            transition: background-color 0.3s, transform 0.2s;
+            position:relative;
+            top: 0px;
+            left: 150px;
+        }
+
+        .contenidoCuadro1 .botones3{
+            width: 540px;  
+            height: 100px;  
+            border: none;  
+            font-size: 32px;  
+            font-weight: bold;
+            cursor: pointer;
+            text-transform: none; 
+            color: black; 
+            background-color:white;
+            border-radius: 5px; 
+            transition: background-color 0.3s, transform 0.2s;
+            position:relative;
+            top: 50px;
+        
+        }
+
+        .contenidoCuadro1 .botones4{
+            width: 267px;  
+            height: 100px;  
+            border: none;  
+            font-size: 32px;  
+            font-weight: bold;
+            cursor: pointer;
+            text-transform: none; 
+            color: black; 
+            background-color:white;
+            border-radius: 5px; 
+            transition: background-color 0.3s, transform 0.2s;
+            position:relative;
+            top: 100px;
+            left: -150px;
+        }
+
+        .contenidoCuadro1 .botones5{
+            width: 267px;  
+            height: 100px;  
+            border: none;  
+            font-size: 32px;  
+            font-weight: bold;
+            cursor: pointer;
+            text-transform: none; 
+            color: black; 
+            background-color:white;
+            border-radius: 5px; 
+            transition: background-color 0.3s, transform 0.2s;
+            position:relative;
+            top: 0px;
+            left: 150px;
+        }
+
+
+        .contenidoCuadro1 .botones6{
+            width: 267px;  
+            height: 100px;  
+            border: none;  
+            font-size: 32px;  
+            font-weight: bold;
+            cursor: pointer;
+            text-transform: none; 
+            color: black; 
+            background-color:white;
+            border-radius: 5px; 
+            transition: background-color 0.3s, transform 0.2s;
+            position:relative;
+            top: 50px;
+            left: -150px;
+        }
+
+        .contenidoCuadro1 .botones7{
+            width: 267px;  
+            height: 100px;  
+            border: none;  
+            font-size: 32px;  
+            font-weight: bold;
+            cursor: pointer;
+            text-transform: none; 
+            color: black; 
+            background-color:white;
+            border-radius: 5px; 
+            transition: background-color 0.3s, transform 0.2s;
+            position:relative;
+            top: -50px;
+            left: 150px;
+        }
+
+
+/*Para el cuadro de la izq ahora */
+
+.contenidoCuadro2 {
             width: 637px;
             height: 740px;
             background-color: #1A428A; 
@@ -44,20 +179,6 @@
             border-radius: 0px;
         }
 
-        .contenidoCuadro1 .titulo1{
-
-            margin: 2;
-            font-size: 48px;
-            font-weight: 700;
-            margin-bottom: 10px;
-            position: relative;
-            left: 0px;
-            top: -280px;
-            color: white;
-        }
-
-
-
         .contenidoCuadro2 .titulo2{
 
             margin: 2;
@@ -65,10 +186,82 @@
             font-weight: 700;
             margin-bottom: 10px;
             position: relative;
-            top: -280px;
+            top: -80px;
             left: 0px;
             color:white;
         }
+
+        .contenidoCuadro2 .botones8{
+            width: 572px;  
+            height: 100px;  
+            border: none;  
+            font-size: 32px;  
+            font-weight: bold;
+            cursor: pointer;
+            text-transform: none; 
+            color: black; 
+            background-color:white;
+            border-radius: 0px; 
+            transition: background-color 0.3s, transform 0.2s;
+            position:relative;
+            top: -50px;
+        
+        }
+
+        .contenidoCuadro2 .botones9{
+            width: 572px;  
+            height: 100px;  
+            border: none;  
+            font-size: 32px;  
+            font-weight: bold;
+            cursor: pointer;
+            text-transform: none; 
+            color: black; 
+            background-color:white;
+            border-radius: 0px; 
+            transition: background-color 0.3s, transform 0.2s;
+            position:relative;
+            top: 0px;
+        
+        }
+
+        .contenidoCuadro2 .botones10{
+            width: 572px;  
+            height: 100px;  
+            border: none;  
+            font-size: 32px;  
+            font-weight: bold;
+            cursor: pointer;
+            text-transform: none; 
+            color: black; 
+            background-color:white;
+            border-radius: 0px; 
+            transition: background-color 0.3s, transform 0.2s;
+            position:relative;
+            top: 50px;
+        
+        }
+
+
+        .contenidoCuadro2 .botones11{
+            width: 572px;  
+            height: 100px;  
+            border: none;  
+            font-size: 32px;  
+            font-weight: bold;
+            cursor: pointer;
+            text-transform: none; 
+            color: black; 
+            background-color:white;
+            border-radius: 0px; 
+            transition: background-color 0.3s, transform 0.2s;
+            position:relative;
+            top: 100px;
+        
+        }
+
+
+
 
         </style>
 </head>
@@ -76,10 +269,62 @@
 
 <div class="contenidoCuadro1">
  <div class="titulo1">ACCESOS DIRECTOS</div>
+ <a href="">
+                    <button class="botones1">Gestión de<br>
+                        Servicios</button>
+                    </a>
+
+ <a href="">
+                    <button class="botones2">Gestión de<br>
+                        tienda online</button>
+                    </a>
+
+ <a href="">
+                    <button class="botones3">Gestión de contenidos <br>
+                        de la empresa</button>
+                    </a>
+
+ <a href="">
+                    <button class="botones4">Gestión de<br>
+                        citas</button>
+                    </a>
+
+
+ <a href="">
+                    <button class="botones5">Gestión de<br>
+                        usuario</button>
+                    </a>
+
+
+
+                    <a href="">
+                    <button class="botones6">Gestión de<br>
+                        Categoría Blog</button>
+                    </a>
+
+
+ <a href="">
+                    <button class="botones7">Gestión de<br>
+                        Artículos Blog</button>
+                    </a>
 </div>
 
 <div class="contenidoCuadro2">
     <div class="titulo2">GESTIÓN DE ACCESOS</div>
+
+                    <button class="botones8">Resumen de accesos a la web</button>
+       
+
+                    <button class="botones9">Resumen de compras de la web</button>
+                 
+
+                    <button class="botones10">Resumen de citas reservadas</button>
+       
+
+                    <button class="botones11">Resumen de nuevas reseñas</button>
+
+
+
 </div>
 
 </body>
